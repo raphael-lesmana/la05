39b6e8be1de3cb502e6e3821e937e616f7aa6bd6
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2022-11-09 15:31:52 +0700

    Update Main.java

--- src/Assingment/Main.java
+++ src/Assingment/Main.java
@@ -5,15 +5,15 @@ import java.time.LocalDateTime;
 public class Main {
 
 	public Main() {
-		Persona persona = new Persona("John Doe", "696969696", "Jl Nowhere, Bogor", "example@example.com");
-		Murid murid = new Murid("Student Doe", "69696969", "Jl Everywhere, Bogor", "example2@example.com",
+		Persona persona = new Persona("John Doe", "Jl Nowhere, Bogor", "01251251251", "example@example.com");
+		Murid murid = new Murid("Student Doe", "Jl Everywhere, Bogor", "9288819", "example2@example.com",
 				"sophomore");
-		Pegawai pegawai = new Pegawai("Part-time Doe", "1699", "Jl Somwehere, Bogor", "example3@example.com",
+		Pegawai pegawai = new Pegawai("Part-time Doe", "Jl Somwehere, Bogor", "1699", "example3@example.com",
 				"Secretary or something", 100000, LocalDateTime.now());
-		Dosen dosen = new Dosen("Teacher Doe", "10000", "Jl. Always, Bogor", "rick_astley@example.com",
+		Dosen dosen = new Dosen("Teacher Doe", "Jl. Always, Bogor", "10000", "rick_astley@example.com",
 				"Teacher of things", 1000, LocalDateTime.now(), "07-17", "Lucasian Professor");
-		Staff staff = new Staff("Labor Hours Doe", "123456", "Jl Rick Astley, Bogor", "help@example.com",
-				"Wonder Boy", 1000, LocalDateTime.now());
+		Staff staff = new Staff("Labor Hours Doe", "Jl Rick Astley, Bogor", "123456", "help@example.com",
+				"He works here", 1000, LocalDateTime.now());
 		System.out.println(staff);
 	}
 
