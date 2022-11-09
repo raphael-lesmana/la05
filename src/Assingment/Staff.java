c2d5b094a64550e041ef9a18ff6c3b68dd137f3e
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2022-11-09 10:50:16 +0700

    Update Staff.java
    aaaaaaaaaaaaaahelpme

--- src/Assingment/Staff.java
+++ src/Assingment/Staff.java
@@ -6,11 +6,10 @@ public class Staff extends Pegawai {
 	public Staff(String nama, String address, String phone_number, String email_address, String office, double salary,
 			LocalDateTime tanggal_rekrut) {
 		super(nama, address, phone_number, email_address, office, salary, tanggal_rekrut);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String toString() {
-		return this.getNama();
+		return this.getClass().getSimpleName() + " " + this.getNama();
 	}
 }
